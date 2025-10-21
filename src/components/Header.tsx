e4913a625f02da2b0da001daa1a6bac0565cd29b
muzammil-gitdev <muzammil.uiit@gmail.com>
2025-10-21 11:11:48 -0700

    final

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,30 @@
 import { Menu } from "lucide-react";
+import { useState } from "react";
 
 export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   const navLinks = ["Home", "Projects", "About", "Skills", "Services"];
   return (
     <header className="header-container">
       <div className="header-inside-container">
         <nav className="nav-container">
           <img className="nav-logo" src="logo.png" alt="" />
-          <ul className="nav-links-container">
+          <ul
+            className={
+              isNavOpen ? "nav-links-container nav-open" : "nav-links-container"
+            }
+          >
             {navLinks.map((curr, i) => (
               <li key={i} className="nav-links">
                 {curr}
               </li>
             ))}
           </ul>
-          <Menu className="btn-nav-open" />
+          <Menu className="btn-nav-open" onClick={toggleNav} />
           <button className="btn btn-cta">Get in Touch</button>
         </nav>
       </div>
